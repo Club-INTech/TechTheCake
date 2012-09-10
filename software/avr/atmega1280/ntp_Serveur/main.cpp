734626df9aec205dbef27e93324c041219112ea2
Nils Darphin <nils.darphin@it-sudparis.eu>
2012-09-10 23:38:47 +0200

    Avancement du code pour le ntp

--- software/avr/atmega1280/ntp_Serveur/main.cpp
+++ software/avr/atmega1280/ntp_Serveur/main.cpp
@@ -2,6 +2,7 @@
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <libintech/timer.hpp>
 
 // LIBRAIRIE INTECH
 #include <libintech/serial/serial_0_interrupt.hpp>
@@ -23,9 +24,11 @@
  *   MODES DE CONFIGURATION     *   
  ********************************/
 
+
+typedef Timer<1,ModeCounter,256> clock;
+
 void setup();
 void synchronisation();
-
 
 int main()
 {
@@ -36,7 +39,6 @@ int main()
     while (1)
         
     {
-
 	char buffer[17];
 	Serial<0>::read(buffer);
 
@@ -47,12 +49,12 @@ int main()
 		Serial<0>::print("Hello World!");
 	}
 	
+	//Demande de synchronisation
 	if( strcmp(buffer, "a") == 0 )
 	{
-		Serial<0>::print("Synchronisation");
+		Serial<0>::print("Test de Synchronisation");
 		synchronisation();
 	}
-	
 	
     }
     return 0;
@@ -69,19 +71,31 @@ void setup()
 	
 	Serial<1>::init();
 	Serial<1>::change_baudrate(9600);
+	
+	//Timers
+	clock::init();
 
 }
 
 void synchronisation()
 {
     char buffer1[17];
+    Serial<0>::print("Tentative de ping...");
     Serial<1>::print_noln("?");
     Serial<1>::read(buffer1);
-    if(strcmp(buffer1, "O") == 0)
+    if(strcmp(buffer1, "!") == 0)
     {
-	Serial<0>::print("Success");
+	Serial<0>::print("ping succeed");
+	Serial<1>::print_noln(clock::value());
+	Serial<1>::read(buffer1);
+	Serial<0>::print(buffer1);
     }
-    else Serial<0>::print("Fail");
+    else Serial<0>::print("Erreur, réponse incorrecte!");
     
 }
 
+
+ISR(TIMER1_OVF_vect)
+{
+	//Rien
+}
