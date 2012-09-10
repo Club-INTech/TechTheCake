6558aabc5f844f9501962a21cba51668a27d9550
Marc <marc.blanc-patin@it-sudparis.eu>
2012-09-10 19:21:12 +0200

    Unification des read Read string amélioré

--- software/avr/atmega328p/print_serial/main.c
+++ software/avr/atmega328p/print_serial/main.c
@@ -1,20 +1,71 @@
 #include <libintech/serial/serial_0_interrupt.hpp>
 #include <libintech/serial/serial_0.hpp>
+#include <util/delay.h>
 #include <avr/io.h>
-#include <stdint.h>  
+#include <stdint.h>
 
 int main() 
 {
- 	//Initialisation série
-	Serial<0>::init();
-	Serial<0>::change_baudrate(9600);
-	sei();
-	
-	while(1){
-		int32_t buf;
-		Serial<0>::read(buf);
-		Serial<0>::print(buf);
-	}
-	
-	return 0;
+    //Initialisation série
+    Serial<0>::init();
+    Serial<0>::change_baudrate(9600);
+    sei();
+    
+    // ------------------
+    // Test des print
+    // ------------------
+    /*
+    // Envoie une chaine
+    char string[10] = "char*";
+    Serial<0>::print(string);
+        
+    _delay_ms(500);
+
+    // Envoie un uint8_t
+    // Envoie le caractère ASCI correspondant
+    uint8_t uint8 = 123;
+    Serial<0>::print(uint8);
+        
+    _delay_ms(500);
+        
+    // Envoie un booléen
+    bool test = true;
+    Serial<0>::print(test);
+        
+    _delay_ms(500);
+
+    // Envoie un int
+    Serial<0>::print(123);
+        
+    _delay_ms(500);
+    Serial<0>::print("-----------");
+    _delay_ms(500);
+    */
+    
+    // ------------------
+    // Test des read
+    // ------------------
+    
+    while(1)
+    {
+        // Réception d'une chaine
+        Serial<0>::print("string:");
+        char buffer[10];
+        Serial<0>::read(buffer);
+        Serial<0>::print(buffer);
+        
+        // Réception d'un float
+        Serial<0>::print("float:");
+        float float_val;
+        Serial<0>::read(float_val);
+        Serial<0>::print(float_val);
+        
+        // Réception d'un int
+        Serial<0>::print("int:");
+        uint8_t uint8_val;
+        Serial<0>::read(uint8_val);
+        Serial<0>::print(uint8_val);
+    }
+    
+    return 0;
 }
--- software/avr/library/libintech/serial/serial_impl.hpp
+++ software/avr/library/libintech/serial/serial_impl.hpp
@@ -6,7 +6,7 @@
  */
 
 #ifndef SERIAL_HPP
-#define	SERIAL_HPP
+#define SERIAL_HPP
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -18,12 +18,12 @@
 #define rx_buffer__SIZE 32
 
 template<uint8_t id>
-class Serial{
+class Serial {
+
 private:
     struct ring_buffer
     {
-        ring_buffer(){
-        }
+        ring_buffer() {}
         unsigned char buffer[rx_buffer__SIZE] ;
         int head;
         int tail;
@@ -38,46 +38,69 @@ private:
 
     static inline bool available(void)
     {
-    		return (rx_buffer__SIZE + rx_buffer_.head - rx_buffer_.tail) % rx_buffer__SIZE;
+        return (rx_buffer__SIZE + rx_buffer_.head - rx_buffer_.tail) % rx_buffer__SIZE;
     }
 
-    static inline void send_ln(){
-	send_char('\r');
-    	send_char('\n');
+    static inline void send_ln()
+    {
+        send_char('\r');
+        send_char('\n');
     }
 
-
-    static inline unsigned char read_single_char(){
-    while(!available()){ asm("nop"); }
-    unsigned char c = rx_buffer_.buffer[rx_buffer_.tail];
-    rx_buffer_.tail = (rx_buffer_.tail + 1) % rx_buffer__SIZE;
-    return c;
+    static inline unsigned char read_single_char()
+    {
+        while(!available()) {}
+        unsigned char c = rx_buffer_.buffer[rx_buffer_.tail];
+        rx_buffer_.tail = (rx_buffer_.tail + 1) % rx_buffer__SIZE;
+        return c;
     }
 
 public:
 
+    /**
+     * Initialise la liaison série (registres)
+     * 
+     */
     static inline void init();
 
+    /**
+     * Fixe le baudrate (défaut à 57600)
+     * 
+     */
     static inline void change_baudrate(uint32_t BAUD_RATE);
 
+    /**
+     * Envoie un caractère sur TX
+     * 
+     */
     static inline void send_char(unsigned char byte);
 
+    /**
+     * Enregistre un caractère dans le buffer, appelée par les interruptions sur RX
+     * 
+     */
     static inline void store_char(unsigned char c)
     {
-    	int i = (rx_buffer_.head + 1) % rx_buffer__SIZE;
-    	if (i != rx_buffer_.tail)
-    	{
-    		rx_buffer_.buffer[rx_buffer_.head] = c;
-    		rx_buffer_.head = i;
-    	}
+        int i = (rx_buffer_.head + 1) % rx_buffer__SIZE;
+        if (i != rx_buffer_.tail)
+        {
+            rx_buffer_.buffer[rx_buffer_.head] = c;
+            rx_buffer_.head = i;
+        }
     }
 
+    /**
+     * Envoie une chaine correspondant à la valeur binaire du paramètre
+     * 
+     */
     template<class T>
-    static inline void print_binary(T val){
+    static inline void print_binary(T val)
+    {
         static char buff[sizeof(T) * 8 + 1];
-	buff[sizeof(T) * 8]='\0';
+        buff[sizeof(T) * 8]='\0';
         int16_t j = sizeof(T) * 8 - 1;
-        for(int16_t i=0 ; i<sizeof(T)*8 ; ++i){
+        for(int16_t i=0 ; i<sizeof(T)*8 ; ++i)
+        {
             if(val & ((T)1 << i))
                buff[j] = '1';
             else
@@ -87,40 +110,69 @@ public:
         print((const char *)buff);
     }
 
-    static inline void print_binary(unsigned char * val, int16_t len){
-        for(int16_t i = 0 ; i<len ; ++i){
-		print_binary(val[i]);
-	}
+    static inline void print_binary(unsigned char * val, int16_t len)
+    {
+        for(int16_t i = 0 ; i<len ; ++i)
+        {
+            print_binary(val[i]);
+        }
     }
 
+    /**
+     * Envoie sur la liaison série, sans retour à la ligne \n (pour communications entre AVR)
+     * 
+     */
     template<class T>
-    static inline void print_noln(T val){
-    	char buffer[10];
+    static inline void print_noln(T val)
+    {
+        char buffer[10];
         ltoa(val,buffer,10);
-    	print_noln((const char *)buffer);
+        print_noln((const char *)buffer);
     }
 
-    static inline void print_noln(char val){
-    	send_char(val);
-    	send_char('\r');
-    }
-
-    static inline void print_noln(unsigned char val){
+    static inline void print_noln(char val)
+    {
         send_char(val);
         send_char('\r');
     }
 
-    static inline void print_noln(const char * val)
+    static inline void print_noln(unsigned char val)
     {
-    	for(uint16_t i = 0 ; i < strlen(val) ; i++)
-    	{
-    		send_char(val[i]);
-    	}
-    	send_char('\r');
+        send_char(val);
+        send_char('\r');
+    }
+    
+    static inline void print_noln(bool val)
+    {
+        (val) ? print_noln("true") : print_noln("false");
+        send_char('\r');
     }
 
+    static inline void print_noln(char* val)
+    {
+        for(uint16_t i = 0 ; i < strlen(val) ; i++)
+        {
+            send_char(val[i]);
+        }
+        send_char('\r');
+    }
+    
+    static inline void print_noln(const char* val)
+    {
+        for(uint16_t i = 0 ; i < strlen(val) ; i++)
+        {
+            send_char(val[i]);
+        }
+        send_char('\r');
+    }
+
+    /**
+     * Envoie sur la liaison série, avec retour à la ligne \n
+     * 
+     */
     template<class T>
-    static inline void print(T val){
+    static inline void print(T val)
+    {
         print_noln(val);
         send_char('\n');
     }
@@ -128,64 +180,88 @@ public:
     // READ INT8_t
     static inline void read(int8_t& valeur){
         static char buffer[20];
-        buffer[read(buffer,20)] = '\0';
+        read(buffer);
         valeur = atol(buffer);
     }
 
     // READ UINT8_t
     static inline void read(uint8_t& valeur){
         static char buffer[20];
-        buffer[read(buffer,20)] = '\0';
+        read(buffer);
         valeur = atol(buffer);
     }
     
     // READ INT16_t
     static inline void read(int16_t& valeur){
         static char buffer[20];
-        buffer[read(buffer,20)] = '\0';
+        read(buffer);
         valeur = atol(buffer);
     }
     
     // READ UINT16_t
     static inline void read(uint16_t& valeur){
         static char buffer[20];
-        buffer[read(buffer,20)] = '\0';
+        read(buffer);
         valeur = atol(buffer);
     }
     
     // READ INT32_t
     static inline void read(int32_t& valeur){
         static char buffer[20];
-        buffer[read(buffer,20)] = '\0';
+        read(buffer);
         valeur = atol(buffer);
     }
     
     // READ UINT32_t
     static inline void read(uint32_t& valeur){
         static char buffer[20];
-        buffer[read(buffer,20)] = '\0';
+        read(buffer);
         valeur = atol(buffer);
     }    
 
     // READ FLOAT
-    static inline void read(float& valeur){
+    static inline void read(float &valeur)
+    {
         static char buffer[20];
-        buffer[read(buffer,20)] = '\0';
+        read(buffer);
         valeur = atof(buffer);
+    }
+    
+    /**
+     * Ecoute sur la liaison série, l'appel est bloquant jusqu'à la réception du délimiteur \r
+     * 
+     */
+    static inline uint8_t read(char* string)
+    {
+        uint8_t i = 0;
+        
+        // Ecoute jusqu'à réception du délimiteur \r
+        do
+        {
+            string[i] = static_cast<char>(read_single_char());
+            i++;
+        }
+        while(string[i-1] != '\r');
+        
+        // Remplace le délimiteur par une fin de chaine
+        string[i-1] = '\0';
+        
+        // Retourne la taille de la chaine lue
+        return i-1;
     }
     
     static inline uint8_t read(unsigned char* string, uint8_t length)
     {
-    	uint8_t i = 0;
-    	for (; i < length; i++)
+        uint8_t i = 0;
+        for (; i < length; i++)
         {
-        	unsigned char tmp = read_single_char();
-        	if(tmp == '\r')
+            unsigned char tmp = read_single_char();
+            if(tmp == '\r')
             {
-        		return i;
+                return i;
             }
             
-        	string[i] = tmp;
+            string[i] = tmp;
         }
         
         return i;
@@ -199,7 +275,7 @@ public:
             char tmp = static_cast<char>(read_single_char());
             if(tmp == '\r'){
                 return i;
-	    }
+        }
             string[i] = tmp;
         }
         return i;
@@ -210,6 +286,6 @@ public:
 template<uint8_t ID>
 volatile typename Serial<ID>::ring_buffer Serial<ID>::rx_buffer_;
 
-#endif	/* SERIAL_HPP */
+#endif  /* SERIAL_HPP */
 
 
