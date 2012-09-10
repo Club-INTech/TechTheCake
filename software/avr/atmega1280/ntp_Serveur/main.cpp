73df590100c41311dbbf5e0ebf1932352be1719f
Nils Darphin <nils.darphin@it-sudparis.eu>
2012-09-10 19:23:42 +0200

    avancement projet

--- software/avr/atmega1280/ntp_Serveur/main.cpp
+++ software/avr/atmega1280/ntp_Serveur/main.cpp
@@ -6,6 +6,8 @@
 // LIBRAIRIE INTECH
 #include <libintech/serial/serial_0_interrupt.hpp>
 #include <libintech/serial/serial_0.hpp>
+#include <libintech/serial/serial_1_interrupt.hpp>
+#include <libintech/serial/serial_1.hpp>
 
 // LIBRAIRIES LOCALES
 
@@ -15,6 +17,9 @@
  ********************************/
 
 #define BAUD_RATE_SERIE         9600
+#define COMPARE_BUFFER(string,len) strncmp(buffer, string, len) == 0 && len>0
+#define COMPARE_BUFFER1(string,len) strncmp(buffer1, string, len) == 0 && len>0
+
 
 /******************************** 
  *   MODES DE CONFIGURATION     *   
@@ -23,8 +28,6 @@
 void setup();
 void synchronisation();
 
-
-typedef Serial<0> serial_t_;
 
 int main()
 {
@@ -39,7 +42,7 @@ int main()
 	char buffer[17];
 	Serial<0>::read(buffer,17);
 
-	#define COMPARE_BUFFER(string,len) strncmp(buffer, string, len) == 0 && len>0
+	
 	//Ping
 	if(COMPARE_BUFFER("?",1))
 	{
@@ -48,7 +51,7 @@ int main()
 	
 	if(COMPARE_BUFFER("a",1))
 	{
-		Serial<0>::print("Synchronisation...");
+		Serial<0>::print("Synchronisation");
 		synchronisation();
 	}
 	
@@ -65,12 +68,22 @@ void setup()
 	//Initialisation série
 	Serial<0>::init();
 	Serial<0>::change_baudrate(9600);
+	
+	Serial<1>::init();
+	Serial<1>::change_baudrate(9600);
 
 }
 
 void synchronisation()
 {
+    char buffer1[17];
+    Serial<1>::print_noln("?");
+    Serial<1>::read(buffer1,17);
+    if(COMPARE_BUFFER1("O",1))
+    {
+	Serial<0>::print("Success");
+    }
+    else Serial<0>::print("Fail");
     
-  
 }
 
