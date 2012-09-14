c51e50e889af71d01e7d04dc277feeb728cfca95
Deboc <pierre@debeaucorps.com>
2012-09-14 19:22:56 +0200

    new code asserv

--- software/avr/atmega328p/asservissement/main.cpp
+++ software/avr/atmega328p/asservissement/main.cpp
@@ -14,15 +14,12 @@
 #include "robot.h"
 
 
-// int8_t i = 0;
-
 int main()
 {
     Robot & robot = Robot::Instance();
 	while(1)
 	{
  		robot.communiquer_pc();
-		
 	}
 	return 0;
 }
@@ -30,18 +27,24 @@ int main()
 ISR(TIMER1_OVF_vect, ISR_NOBLOCK){
 	Robot & robot = Robot::Instance();
 	
+	
 	if (robot.BASCULE())
 		robot.bandeArcade();
 	else
 	{
+		//mise à jour des attribut stockant la distance parcourue en tic et l'angle courant en tic
 		int32_t infos[2];
-		//info[0]=>distance courante ; info[1] => angle courant.
 		get_all(infos);
-
 		robot.mesure_distance(infos[0]);
 		robot.mesure_angle(infos[1]);
+		
+		//mise à jour du pwm envoyé aux moteurs pour l'asservissement
 		robot.asservir();
+		
+		//vérification des conditions de bloquage du robot
 		robot.gestion_blocage();
+		
+		//calcul de la nouvelle position courante du robot, en absolu sur la table (mm et radians)
 		robot.update_position();
 	}
 }
