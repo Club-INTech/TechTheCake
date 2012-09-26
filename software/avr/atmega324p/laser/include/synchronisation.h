49ad83d9967c43a51af0c5a09115406b73fc2cd8
Nils Darphin <nils.darphin@it-sudparis.eu>
2012-09-26 18:21:21 +0200

    Nouvelle branche pour le ntp

---
+++ software/avr/atmega324p/laser/include/synchronisation.h
@@ -0,0 +1,28 @@
+#ifndef SYNCHRONISATION_H
+#define SYNCHRONISATION_H
+
+#include <stdint.h>
+#include <string.h>
+
+
+
+template<class Timer , class Serial>
+class Synchronisation
+{
+
+private:
+    volatile uint32_t clock_;
+    
+public:
+    Synchronisation();
+    
+    void synchroniser();
+    
+    void interruption();
+    
+    uint32_t clock();
+    
+  
+};
+
+#endif
