c5f1498f5d18aef35d69807ebec6a4a45323963d
Deboc <pierre@debeaucorps.com>
2012-10-03 00:27:10 +0200

    classe Conteneur

---
+++ software/pc/conteneur.py
@@ -0,0 +1,43 @@
+
+#importation de la configuration
+from src.read_ini import Config,Constantes
+
+#module d'injection de dépendances
+from src.assemblage import assembler
+
+#modules
+from src.robot import Robot
+from src.deplacements import Deplacements_simu, Deplacements_serie
+
+class Container:
+    def __init__(self):
+        self.assembler = assembler()
+        
+        #enregistrement du service de configuration
+        def make_conf():
+            conf = Config()
+            conf.set_chemin("config")
+            return conf
+        self.assembler.register(Config,factory=make_conf)
+        
+        #utilisation du service de configuration pour la suite
+        self.config = self.get_service(Config)
+        
+        #enregistrement du service des déplacements
+        if (self.config["mode_simulateur"]):
+            print("mode simulateur")
+            Deplacements = Deplacements_simu
+        else:
+            print("mode série")
+            Deplacements = Deplacements_serie
+        self.assembler.register(Deplacements, requires=[Config])
+        
+        #enregistrement du service robot
+        self.assembler.register(Robot, requires=[Deplacements,Config])
+        
+    def get_service(self,type):
+        return self.assembler.provide(type)
+        
+        
+container = Container()
+robot = container.get_service(Robot)
