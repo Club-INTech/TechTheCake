8a77ee365811aad840e48917a945619681dee55d
PF <pierre-francois.gimenez@telecom-sudparis.eu>
2012-11-07 22:00:10 +0100

    Code python médiane

--- software/pc/src/capteurs.py
+++ software/pc/src/capteurs.py
@@ -15,10 +15,14 @@ class Capteurs():
         """
         renvoi une distance, toussa...
         """
+#        self.aze = "zer" #variable pour toute la classe
+#        aze = "zer"	#variable pour la fonction
         
+        self.log.debug("ca va")
+        self.log.warning("probleme")
+        self.log.critical("gros probleme")
         
-        
-        
+
         """
         pour PF : on utilise ici la méthode "communiquer" du service "serie" qui est en attribut de la classe Capteurs.
         elle prend trois arguments : 
@@ -28,6 +32,25 @@ class Capteurs():
         cette méthode renvoit un TABLEAU de STRING, contenant les lignes de retour.
         Donc on oublie pas de bien caster la sortie :)
         """
-        retour = self.serie.communiquer("capteurs_actionneurs",["mesurer"], 1)
-        return int(retour[0])
-    
+
+        m=3 #le nombre de valeurs d'où est extraite la médiane
+        valeurs=[] #cette liste contiendra les m valeurs
+        for i in range(m):
+            retour = self.serie.communiquer("capteurs_actionneurs",["s"], 1)
+            valeurs.append(int(retour[0]))
+
+        valeurs.sort()	#les valeurs sont triées dans l'ordre croissant
+
+        capteurUltrason=valeurs[m//2]
+
+        valeurs=[]
+        for i in range(m): #idem, mais avec les infrarouges
+            retour = self.serie.communiquer("capteurs_actionneurs",["i"], 1)
+            valeurs.append(int(retour[0]))
+
+        valeurs.sort()
+
+        capteurInfrarouge=valeurs[m//2]
+
+        return max(capteurInfrarouge, capteurUltrason)
+
