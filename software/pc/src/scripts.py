c9bd25aea6543b6fc2c1eb987d1f5edf0f39fe9e
Deboc <pierre@debeaucorps.com>
2012-12-16 18:25:29 +0100

    script de test pour les cadeaux

--- software/pc/src/scripts.py
+++ software/pc/src/scripts.py
@@ -61,3 +61,18 @@ class ScriptTestHooks(Script):
         self.robot.gestion_avancer(300,hooks)
         self.robot.gestion_tourner(pi/2,hooks)
         self.robot.gestion_avancer(500,hooks)
+        
+        
+class ScriptTestCadeaux(Script):
+    
+    def execute(self):
+        
+        
+        self.robot.gestion_va_au_point(1000,200)
+
+        hooks = []
+        hooks.append(self.hookGenerator.get_hook("position", Point(910,300), self.robot.ouvrir_cadeau))
+        hooks.append(self.hookGenerator.get_hook("position", Point(910,300), self.robot.fermer_cadeau))
+        
+        self.robot.gestion_avancer(300,hooks)
+        self.robot.gestion_tourner(pi/2,hooks)
