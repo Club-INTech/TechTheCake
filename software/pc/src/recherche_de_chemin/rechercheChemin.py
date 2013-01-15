733c76607d7f1fa1dd0160d55405c4553a6421bf
Deboc <pierre@debeaucorps.com>
2013-01-15 13:57:29 +0100

    Meilleure structure.

--- software/pc/src/recherche_de_chemin/rechercheChemin.py
+++ software/pc/src/recherche_de_chemin/rechercheChemin.py
@@ -22,48 +22,9 @@ class Environnement:
         self.cercles = []
         self.polygones = []
         
-class RechercheChemin:
-    
-    def __init__(self,table,config,log):
-        
-        #services nécessaires
-        self.table = table
-        self.config = config
-        self.log = log
-        
-        # tolerance de précision (différent de 0.0)
-        #self.tolerance = 0.0000001
-        self.tolerance = 0.001
-        
         # côté des polygones qui représentent des cercles, en mm (petit : précision, grand : complexité moindre)
         self.cote_polygone = 100
-        
-        # environnement initial : bords de la map en 1er élément, puis obstacles fixes
-        self.environnement_initial = Environnement()
-        # bords de la carte : doit être le premier élément de la liste
-        self._ajoute_rectangle_initial(vis.Polygon([vis.Point(-1500,0), vis.Point(1500,0), vis.Point(1500,2000), vis.Point(-1500,2000)]))
-        # Définition des polygones des obstacles fixes. Ils doivent être non croisés et définis dans le sens des aiguilles d'une montre.
-        self._ajoute_rectangle_initial(vis.Polygon([vis.Point(100, 300),vis.Point(100, 500),vis.Point(150, 500),vis.Point(150, 300)]))
-        self._ajoute_rectangle_initial(vis.Polygon([vis.Point(0, 875),vis.Point(0, 1125),vis.Point(775, 1125),vis.Point(775, 875)]))
-        self._ajoute_rectangle_initial(vis.Polygon([vis.Point(-775, 875),vis.Point(-775, 1125),vis.Point(-525, 1125),vis.Point(-525, 875)]))
-        
-        # environnement dynamique : liste des obstacles mobiles qui sont mis à jour régulièrement
-        self.environnement_dynamique = Environnement()
-        
-        
-    def _collision_polygone_point(self,polygone,point):
-        """
-        Test de collision pour l'accessibilité du point d'arrivé
-        """
-        def test_segment(a,b):
-            if ((b.x-a.x)*(point.y-a.y) - (b.y-a.y)*(point.x-a.x) > 0):
-                return False
-            return True
-        for i in range(polygone.n()-1):
-            if not test_segment(polygone[i],polygone[i+1]): return False
-        if not test_segment(polygone[polygone.n()-1],polygone[0]): return False
-        return True
-        
+    
     def _polygone_du_cercle(self,cercle):
         """
         méthode de conversion cercle -> polygone
@@ -85,17 +46,57 @@ class RechercheChemin:
         rayon = math.sqrt((rectangle[0].x - rectangle[2].x)**2 + (rectangle[0].y - rectangle[2].y)**2)/2.
         return Cercle(centre,rayon)
         
-    ### Pour l'environnement initial #########
-    def _ajoute_rectangle_initial(self, rectangle):
-        self.environnement_initial.polygones.append(rectangle)
-        self.environnement_initial.cercles.append(self._cercle_circonscrit_du_rectangle(rectangle))
-    ############################################
+    def ajoute_cercle(self, cercle):
+        self.cercles.append(cercle)
+        self.polygones.append(self._polygone_du_cercle(cercle))
+    
+    def ajoute_rectangle(self, rectangle):
+        self.polygones.append(rectangle)
+        self.cercles.append(self._cercle_circonscrit_du_rectangle(rectangle))
+        
+class RechercheChemin:
+    
+    def __init__(self,table,config,log):
+        
+        #services nécessaires
+        self.table = table
+        self.config = config
+        self.log = log
+        
+        # tolerance de précision (différent de 0.0)
+        #self.tolerance = 0.0000001
+        self.tolerance = 0.001
+        
+        # environnement initial : bords de la map en 1er élément, puis obstacles fixes
+        self.environnement_initial = Environnement()
+        # bords de la carte : doit être le premier élément de la liste
+        self.environnement_initial.ajoute_rectangle(vis.Polygon([vis.Point(-1500,0), vis.Point(1500,0), vis.Point(1500,2000), vis.Point(-1500,2000)]))
+        # Définition des polygones des obstacles fixes. Ils doivent être non croisés et définis dans le sens des aiguilles d'une montre.
+        self.environnement_initial.ajoute_rectangle(vis.Polygon([vis.Point(100, 300),vis.Point(100, 500),vis.Point(150, 500),vis.Point(150, 300)]))
+        self.environnement_initial.ajoute_rectangle(vis.Polygon([vis.Point(0, 875),vis.Point(0, 1125),vis.Point(775, 1125),vis.Point(775, 875)]))
+        self.environnement_initial.ajoute_rectangle(vis.Polygon([vis.Point(-775, 875),vis.Point(-775, 1125),vis.Point(-525, 1125),vis.Point(-525, 875)]))
+        
+        # environnement dynamique : liste des obstacles mobiles qui sont mis à jour régulièrement
+        self.environnement_dynamique = Environnement()
+        
+        
+    def _collision_polygone_point(self,polygone,point):
+        """
+        Test de collision pour l'accessibilité du point d'arrivé
+        """
+        def test_segment(a,b):
+            if ((b.x-a.x)*(point.y-a.y) - (b.y-a.y)*(point.x-a.x) > 0):
+                return False
+            return True
+        for i in range(polygone.n()-1):
+            if not test_segment(polygone[i],polygone[i+1]): return False
+        if not test_segment(polygone[polygone.n()-1],polygone[0]): return False
+        return True
         
     ### Pour l'environnement dynamique #########
-    def ajoute_cercle(self, centre, rayon):
+    def ajoute_obstacle_cercle(self, centre, rayon):
         cercle = Cercle(centre,rayon)
-        self.environnement_dynamique.cercles.append(cercle)
-        self.environnement_dynamique.polygones.append(self._polygone_du_cercle(cercle))
+        self.environnement_dynamique.ajoute_cercle(cercle)
             
     def retirer_obstacles_dynamiques(self):
         self.environnement_dynamique.polygones = []
