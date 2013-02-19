00c9c8ff2af9a9269141bf9968033482407a81fe
Pierre-François Gimenez <pierre-francois.gimenez@telecom-sudparis.eu>
2013-02-19 08:57:02 +0100

    Correction gestion des points, appel des scripts avec paramètres

--- software/pc/src/strategie.py
+++ software/pc/src/strategie.py
@@ -20,8 +20,8 @@ class Strategie:
         self.config = config
         self.log = log
 
-        self.arguments={"pipeau1":(), "pipeau2":(), "pipeau3":(), "casser_tour":(), "bougies": (1,)}
-        self.scripts = {"pipeau1":ScriptPipeauStrategie1, "pipeau2":ScriptPipeauStrategie2, "pipeau3":ScriptPipeauStrategie3, "casser_tour":ScriptCasserTour}
+        self.arguments={"pipeau1":(), "pipeau2":(), "pipeau3":(), "casser_tour":(), "bougies": (1,), "cadeaux1":(0,), "cadeaux2":(1,), "testHook":()}
+        self.scripts = {"cadeaux1": ScriptCadeaux, "cadeaux2":ScriptCadeaux, "testHook":ScriptTestHooks} #"casser_tour":ScriptCasserTour
         if not self.config["ennemi_fait_toutes_bougies"]:
             if self.config["ennemi_fait_ses_bougies"]:
                 self.scripts["bougies"]=ScriptBougiesRapide
@@ -32,7 +32,7 @@ class Strategie:
         else:
             self.log.warning("Puisque l'ennemi est scripté et fait toutes les bougies, on ne les fera pas.")
         self.liste_points_entree = ["cadeau", "verreNous", "verreEnnemi", "Pipeau"]
-        self.points={"cadeau":0, "verres": 0, "gateau":0, "bougies":0, "deposer_verre":0, "pipeau1":6, "pipeau2": 12, "pipeau3":8}
+        self.points={"cadeaux1":0, "cadeaux2":0, "verres": 0, "gateau":0, "bougies":0, "deposer_verre":0, "pipeau1":6, "pipeau2": 12, "pipeau3":8, "testHook":0}
 
         for script,classe in self.scripts.items():
             self.scripts[script] = classe()
@@ -47,35 +47,23 @@ class Strategie:
         while not self.timer.get_fin_match():
 #            self.rechercheChemin.retirer_obstacles_dynamique();
 
-            note={"cadeau":0, "verreNous":0, "verreEnnemi": 0, "gateau":0, "bougies":0, "deposer_verres":0, "pipeau1":0, "pipeau2":0, "pipeau3":0, "casser_tour":0} #les clés sont les scripts
+            note={"cadeaux1":0, "cadeaux2":0, "verreNous":0, "verreEnnemi": 0, "gateau":0, "bougies":0, "deposer_verres":0, "pipeau1":0, "pipeau2":0, "pipeau3":0, "casser_tour":0, "testHook":0} #les clés sont les scripts
 
         #        for script in self.points: #retiré pour la durée des tests (tant que les vrais scripts ne sont pas dispo...)
         #            self.points[script]=0
             self.points["deposer_verre"]=6#(nb verre)
-            for element in self.table.bougies:
-                if not element["traitee"]:
-                    self.points["gateau"]+=2 #chaque bougie rapporte 4 points, mais la moitié des bougies sont à l'ennemi...
+ #           for element in self.table.bougies:
+ #               if not element["traitee"]:
+ #                   self.points["gateau"]+=2 #chaque bougie rapporte 4 points, mais la moitié des bougies sont à l'ennemi...
             for element in self.table.verres:
                 if element["present"]:       #à pondérer si l'ascenseur est plutôt plein ou plutôt vide
                     self.points["verres"]+=6 #à tirer vers le haut pour les faire en début de partie (et ensuite baisser les points par incertitude?)
             for element in self.table.cadeaux:
                 if not element["ouvert"]:
-                    self.points["cadeau"]+=4
+                    self.points["cadeaux1"]+=4
+            self.points["cadeaux2"]=self.points["cadeaux1"]
             self.points["deposer_verres"]=4*(self.robot.nb_verres_avant+self.robot.nb_verres_arriere)**2 #mettre la vraie valeur
             self.points["casser_tour"]=(time()-self.timer.get_date_debut())
-
-#            self.points["pipeau2"]=0
-#            for element in self.table.bougies:
-#                if not element["traitee"]:
-#                    self.points["pipeau2"]+=2
-#            self.points["pipeau3"]=0
-#            for element in self.table.bougies:
-#                if not element["traitee"]:
-#                    self.points["pipeau3"]+=2
-#            self.points["pipeau1"]=0
-#            for element in self.table.cadeaux:
-#                if not element["ouvert"]:
-#                    self.points["pipeau1"]+=4
 
             self.rechercheChemin.preparer_environnement()
 
@@ -88,9 +76,10 @@ class Strategie:
                 distanceE=self._distance_ennemi(script)+1              #idem
                 try:
                     note[script]=1000000000*(self.points[script])/(dureeScript*dureeScript*dureeScript*distanceE*distanceE)
+                    self.log.debug("self.points[script]="+str(self.points["cadeaux1"])+" dureeScript= "+str(dureeScript)+" distanceE "+str(distanceE))
                 except ZeroDivisionError:
                     note[script]=self.points[script]
-                    self.log.critical("Division par zéro! :o") #sait-on jamais... je préfère ne pas prendre le risque de voir le robot se paralyser bêtement
+                    self.log.critical("Division par zéro dans le calcul de la note! :o") #sait-on jamais... je préfère ne pas prendre le risque de voir le robot se paralyser bêtement
 
                 if script=="verreNous" or script=="verreEnnemi" and dureeScript+deposer_verre.calcule()>(self.config["temps_match"]-time()+self.timer.get_date_debut()): #pour prendre les verres, on ajoute à durée script le temps de déposer les verres
                     self.log.critical("Plus le temps de prendre des verres, on n'aurait pas le temps de les déposer.")
@@ -108,15 +97,14 @@ class Strategie:
 
             self.log.warning("Stratégie fait: "+scriptAFaire)
             if not self.timer.get_fin_match():
-                self.scripts[scriptAFaire].agit()
-                self.points[scriptAFaire]-=2
+                self.scripts[scriptAFaire].agit(self.arguments[scriptAFaire])
 
             sleep(0.1)
-#        self.log.debug("Arrêt de la stratégie.")
+        self.log.debug("Arrêt de la stratégie.")
 
     def _distance_ennemi(self, script): #on prend la distance euclidienne, à vol d'oiseau. Attention, on prend le min: cette valeur est sensible aux mesures aberrantes
         distance_min=3000 #une distance très grande, borne sup de la valeur renvoyée.
-        for obstacle in self.table.get_robotsAdversesBalise()+self.table.get_obstaclesCapteur():
+        for obstacle in self.table.obstacles():
             delta_x=self.scripts[script].point_entree().x-obstacle.position.x
             delta_y=self.scripts[script].point_entree().y-obstacle.position.y
             d=round(sqrt(delta_x**2 + delta_y**2),2)
