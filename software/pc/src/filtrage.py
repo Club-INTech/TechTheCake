5a841d422538e53e4d82aabaa30148d6117e239c
Marc <marc.blanc-patin@telecom-sudparis.eu>
2013-02-23 18:22:26 +0100

    Amélioration du filtrage

--- software/pc/src/filtrage.py
+++ software/pc/src/filtrage.py
@@ -36,17 +36,18 @@ class FiltrageLaser:
     
     def __init__(self, config):
         self.config = config
-        self.dt = 0.3
+        self.dt = 0.2
         x = numpy.array([1400,100,0.,0.])[:, numpy.newaxis] # vecteur d'état au départ
         P = numpy.matrix([[30.,0.,0.,0.],[0.,30.,0.,0.],[0.,0.,10.,0.],[0.,0.,0.,10.]]) # incertitude initiale
         F = numpy.matrix([[1.,0.,self.dt,0.],[0.,1.,0.,self.dt],[0.,0.,1.,0.],[0.,0.,0.,1.]]) # matrice de transition
         H = numpy.matrix([[1.,0.,0.,0.],[0.,1.,0.,0.]])# matrice d'observation
-        R = numpy.matrix([[30,0.],[0.,30]]) # incertitude sur la mesure
+        R = numpy.matrix([[900,0.],[0.,900]]) # incertitude sur la mesure
         #Q = numpy.matrix([[self.dt**3/3., self.dt**2/2., 0, 0],[self.dt**2/2.,self.dt, 0, 0],[0,0,self.dt**3/3.,self.dt**2/2],[0,0,self.dt**2/2,self.dt]])
         #Q *= 20;
-        Q = numpy.matrix([[3, 0, 0, 0],[0, 3, 0, 0],[0, 0, 3, 0],[0, 0, 0, 3]])
+        Q = numpy.matrix([[1, 0, 0, 0],[0, 1, 0, 0],[0, 0, 4, 0],[0, 0, 0, 4]])
+        Q *= 12
         self.filtre_kalman = Kalman(x, P, F, H, R, Q)
-        self.historique = collections.deque(maxlen=2)
+        self.historique = collections.deque(maxlen=3)
         self.valeurs_rejetees = 0
         self.acceleration = None
         
@@ -54,8 +55,9 @@ class FiltrageLaser:
         return self.filtre_kalman.x
         
     def update_dt(self, new_dt):
+        self.dt = new_dt
+        self.filtre_kalman.F[0,2] = new_dt
         self.filtre_kalman.F[1,3] = new_dt
-        self.filtre_kalman.F[2,4] = new_dt
     
     def position(self):
         state = self.filtre_kalman.x
@@ -76,24 +78,28 @@ class FiltrageLaser:
         Vérifie si le point est cohérent avec la position actuelle, en basant sur l'accélération
         """
         # Pas suffisamment de valeurs précédentes pour calculer l'accélération
-        if len(self.historique) != 2:
+        if len(self.historique) != 3:
             return True
             
-        # 2 derniers points valides
-        pointm1 = self.historique[1]
-        pointm2 = self.historique[0]
+        # 3 derniers points valides
+        pointm1 = self.historique[2]
+        pointm2 = self.historique[1]
+        pointm3 = self.historique[0]
         
         # Vecteurs vitesses et accélération
         vitesse_actuelle = pointm0 - pointm1
-        vitesse_precedente = pointm1 - pointm2
-        acceleration = vitesse_actuelle - vitesse_precedente
+        vitesse_m1 = pointm1 - pointm2
+        vitesse_m2 = pointm2 - pointm3
+        acceleration_actuelle = vitesse_actuelle - vitesse_m1
+        acceleration_precedente = vitesse_m1 - vitesse_m2
+        jerk = acceleration_actuelle - acceleration_precedente
         
         # Produit scalaire pour savoir s'il y a accélération ou décélération
-        produit = acceleration.x * vitesse_precedente.x + acceleration.y * vitesse_precedente.y
+        produit = acceleration_actuelle.x * vitesse_m1.x + acceleration_actuelle.y * vitesse_m1.y
         
         # Rejette les accélérations brutales
-        if acceleration.norme() / self.dt**2 > 1000 and produit > 0 and self.valeurs_rejetees < 3:
-            #print("accélération rejetée {0}, produit = {1}".format(acceleration, produit))
+        if acceleration_actuelle.norme() / self.dt**2 > 50000 and self.valeurs_rejetees < 3:
+            #~ print("accélération = {0}, produit = {1}, jerk = {2}".format(acceleration_actuelle.norme() / self.dt**2, produit, jerk.norme() / self.dt**3))
             self.valeurs_rejetees += 1
             return False
         else:
