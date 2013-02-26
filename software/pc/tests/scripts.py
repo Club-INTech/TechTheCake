d092bc3be2c88a8df0b7c244401a567bed73c9c8
Pierre-François Gimenez <pierre-francois.gimenez@telecom-sudparis.eu>
2013-02-26 12:40:35 +0100

    Tests unitaires du script des verres

--- software/pc/tests/scripts.py
+++ software/pc/tests/scripts.py
@@ -1,4 +1,5 @@
 import tests.tests
+from time import sleep
 
 class TestScriptCadeaux(tests.tests.ContainerTest):
     
@@ -11,13 +12,13 @@ class TestScriptCadeaux(tests.tests.ContainerTest):
         self.assertEqual(scripts_cadeaux.info_versions[0]["point_entree"], table.cadeaux[0]["position"]+scripts_cadeaux.decalage_droit)
         self.assertEqual(scripts_cadeaux.info_versions[1]["point_entree"], table.cadeaux[3]["position"]+scripts_cadeaux.decalage_gauche)
 
-        # Cas où un cadeau extrême gauche a été retiré
+        # Cas où le cadeau extrême gauche a été retiré
         table.cadeau_recupere(table.cadeaux[3])
         self.assertEqual(scripts_cadeaux.versions(), [0, 1])
         self.assertEqual(scripts_cadeaux.info_versions[0]["point_entree"], table.cadeaux[0]["position"]+scripts_cadeaux.decalage_droit)
         self.assertEqual(scripts_cadeaux.info_versions[1]["point_entree"], table.cadeaux[2]["position"]+scripts_cadeaux.decalage_gauche)
 
-        # Cas où un cadeau extrême gauche a été retiré
+        # Cas où le cadeau extrême gauche a été retiré
         table.cadeau_recupere(table.cadeaux[2])
         self.assertEqual(scripts_cadeaux.versions(), [0, 1])
         self.assertEqual(scripts_cadeaux.info_versions[0]["point_entree"], table.cadeaux[0]["position"]+scripts_cadeaux.decalage_droit)
@@ -40,3 +41,93 @@ class TestScriptCadeaux(tests.tests.ContainerTest):
         scripts_cadeaux.agit(0)
         self.assertEqual(table.cadeaux_restants(), [])
         
+
+class TestScriptRecupererVerresZoneRouge(tests.tests.ContainerTest):
+    
+    def test_point_entree(self):
+        table = self.get_service("table")
+        robot = self.get_service("robot")
+        scripts_verres = self.get_service("scripts")["ScriptRecupererVerresZoneRouge"]
+
+        self.assertEqual(scripts_verres.versions(), [0, 1])
+        self.assertEqual(scripts_verres.info_versions[0]["point_entree"], table.verres[0]["position"])
+        self.assertEqual(scripts_verres.info_versions[1]["point_entree"], table.verres[5]["position"])
+
+        table.verre_recupere(table.verres[5])
+        self.assertEqual(scripts_verres.versions(), [0, 1])
+        self.assertEqual(scripts_verres.info_versions[0]["point_entree"], table.verres[0]["position"])
+        self.assertEqual(scripts_verres.info_versions[1]["point_entree"], table.verres[4]["position"])
+
+        table.verre_recupere(table.verres[4])
+        table.verre_recupere(table.verres[3])
+        table.verre_recupere(table.verres[2])
+        table.verre_recupere(table.verres[1])
+        self.assertEqual(scripts_verres.versions(), [0])
+        self.assertEqual(scripts_verres.info_versions[0]["point_entree"], table.verres[0]["position"])
+
+        table.verre_recupere(table.verres[0])
+        self.assertEqual(scripts_verres.versions(), [])
+
+    def test_score(self):
+        table = self.get_service("table")
+        robot = self.get_service("robot")
+        scripts_verres = self.get_service("scripts")["ScriptRecupererVerresZoneRouge"]
+
+        # On prends les six verres (2 et 4)
+        robot.nb_verres_avant = 0
+        robot.nb_verres_arriere = 0
+        self.assertEqual(scripts_verres.score(), 52)
+
+        # Ascenseur avant plein
+        robot.nb_verres_avant = 4
+        robot.nb_verres_arriere = 0
+        self.assertEqual(scripts_verres.score(), 40)
+
+        # Ascenseur arrière plein
+        robot.nb_verres_avant = 0
+        robot.nb_verres_arriere = 4
+        self.assertEqual(scripts_verres.score(), 40)
+
+        # On ne peut prendre que trois verres
+        robot.nb_verres_avant = 3
+        robot.nb_verres_arriere = 2
+        self.assertEqual(scripts_verres.score(), 44)
+
+# Tests pas encore fonctionnels!
+class TestScriptBougies(tests.tests.ContainerTest):
+    
+    def test_point_entree(self):
+        table = self.get_service("table")
+        config = self.get_service("config")
+        scripts_bougies = self.get_service("scripts")["ScriptBougies"]
+
+        # Le temps que le thread des bougies nous donne leur couleur
+        sleep(5)
+
+        # Cas où aucune bougie n'a été soufflée
+        self.assertEqual(scripts_bougies.versions(), [0, 1])
+        self.assertEqual(scripts_bougies.info_versions[0]["angle_entree"], table.bougies[2]["position"] - 20 / float(500 + config["distance_au_gateau"]))
+        self.assertEqual(scripts_bougies.info_versions[1]["angle_entree"], table.bougies[16]["position"] - 20 / float(500 + config["distance_au_gateau"]))
+
+        # Cas où le cadeau extrême gauche a été retiré
+        table.cadeau_recupere(table.cadeaux[3])
+        self.assertEqual(scripts_bougies.versions(), [0, 1])
+        self.assertEqual(scripts_bougies.info_versions[0]["angle_entree"], table.bougies[2]["position"] - 20 / float(500 + config["distance_au_gateau"]))
+        self.assertEqual(scripts_bougies.info_versions[1]["angle_entree"], table.bougies[17]["position"] - 20 / float(500 + config["distance_au_gateau"]))
+
+        # Cas où le cadeau extrême gauche a été retiré
+        table.cadeau_recupere(table.cadeaux[2])
+        self.assertEqual(scripts_bougies.versions(), [0, 1])
+        self.assertEqual(scripts_bougies.info_versions[0]["angle_entree"], table.bougies[2]["position"] - 20 / float(500 + config["distance_au_gateau"]))
+        self.assertEqual(scripts_bougies.info_versions[1]["angle_entree"], table.bougies[17]["position"] - 20 / float(500 + config["distance_au_gateau"]))
+
+        # Cas où il n'y a plus qu'un seul cadeau
+        table.cadeau_recupere(table.cadeaux[1])
+        self.assertEqual(scripts_bougies.versions(), [0, 1])
+        self.assertEqual(scripts_bougies.info_versions[0]["angle_entree"], table.bougies[2]["position"] - 20 / float(500 + config["distance_au_gateau"]))
+        self.assertEqual(scripts_bougies.info_versions[1]["angle_entree"], table.bougies[17]["position"] - 20 / float(500 + config["distance_au_gateau"]))
+
+        # Cas où il n'y a plus de cadeaux
+        table.bougies_recupere(table.cadeaux[0])
+        self.assertEqual(scripts_bougies.versions(), [])
+
