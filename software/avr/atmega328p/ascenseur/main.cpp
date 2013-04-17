9cf791a507c50322c2b1a15e5893ae52e8469f4e
pierre <pierre@kayou.(none)>
2013-04-17 22:31:29 +0200

    L'ascenseur avant va devant, l'ascenseur arriere va derriere

--- software/avr/atmega328p/ascenseur/main.cpp
+++ software/avr/atmega328p/ascenseur/main.cpp
@@ -29,10 +29,10 @@ ISR (TIMER1_OVF_vect, ISR_NOBLOCK)
 	int32_t position[2];
 	Actionneurs::timer_asserv::value(54000); // On met une valeur sur le timer d'asservissement  pour accéder plus rapidement au prochain overflow
 	get_all(position);
-	actionneurs.ascenseur_avant.changerValeurCodeuse(position[0]);
-	actionneurs.ascenseur_arriere.changerValeurCodeuse(position[1]);
-	actionneurs.ascenseur_avant.asservir();
+	actionneurs.ascenseur_arriere.changerValeurCodeuse(position[0]);
+	actionneurs.ascenseur_avant.changerValeurCodeuse(position[1]);
 	actionneurs.ascenseur_arriere.asservir();
+	actionneurs.ascenseur_avant.asservir();
 }
 
 
