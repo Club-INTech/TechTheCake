f8b885ba835d35f29f6aa256e244f64f8abd2889
pierre <pierre@kayou.(none)>
2013-04-18 20:31:27 +0200

    Coté rouge pour la detection de couleur

--- software/android/INTech/jni/model.cpp
+++ software/android/INTech/jni/model.cpp
@@ -12,16 +12,16 @@ map<int, Point2f> Model::getRepartitionModel(char color)
 
 	if (color == 'r')
 	{
-		model.insert(pair<int, Point2f>(1, Point(115, 111)));
-		model.insert(pair<int, Point2f>(2, Point(146, 114)));
-		model.insert(pair<int, Point2f>(3, Point(186, 114)));
-		model.insert(pair<int, Point2f>(4, Point(232, 116)));
-		model.insert(pair<int, Point2f>(5, Point(278, 114)));
-		model.insert(pair<int, Point2f>(6, Point(322, 112)));
-		model.insert(pair<int, Point2f>(7, Point(148, 80)));
-		model.insert(pair<int, Point2f>(8, Point(188, 81)));
-		model.insert(pair<int, Point2f>(9, Point(240, 81)));
-		model.insert(pair<int, Point2f>(10, Point(290, 80)));
+		model.insert(pair<int, Point2f>(1, Point(346, 88)));
+		model.insert(pair<int, Point2f>(2, Point(313, 91)));
+		model.insert(pair<int, Point2f>(3, Point(273, 96)));
+		model.insert(pair<int, Point2f>(4, Point(226, 98)));
+		model.insert(pair<int, Point2f>(5, Point(180, 99)));
+		model.insert(pair<int, Point2f>(6, Point(135, 100)));
+		model.insert(pair<int, Point2f>(7, Point(307, 43)));
+		model.insert(pair<int, Point2f>(8, Point(263, 63)));
+		model.insert(pair<int, Point2f>(9, Point(208, 65)));
+		model.insert(pair<int, Point2f>(10, Point(159, 63)));
 	}
 	else
 	{
