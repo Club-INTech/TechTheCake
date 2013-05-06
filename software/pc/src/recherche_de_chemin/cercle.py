3fa06d2919ad79ed337edd25add74aae578c38b7
Deboc <pierre@debeaucorps.com>
2013-05-06 02:54:21 +0200

    Deuxième ménage de printemps

--- software/pc/src/recherche_de_chemin/cercle.py
+++
@@ -1,9 +0,0 @@
-import recherche_de_chemin.visilibity as vis
-
-class Cercle:
-    def __init__(self,centre,rayon):
-        self.centre = centre
-        self.rayon = rayon
-        
-    def copy(self):
-        return Cercle(vis.Point(self.centre.x, self.centre.y), self.rayon)
